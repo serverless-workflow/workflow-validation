c2bb7f733b5c9483a5612a0f3ccc48acac90bc78
tsurdilo <tsurdilo@redhat.com>
2019-11-13 13:48:58 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Serverless Workflow Validation
+
+Provides Validation (both JSON Schema and Workflow Model) for the
+Serverless Workflow Specification Version 0.1 - https://github.com/cncf/wg-serverless/blob/master/workflow/spec/spec.md
---
+++ src/test/java/org/serverless/workflow/validation/testproviders/TestWorkflowValidator.java
@@ -0,0 +1,63 @@
+/*
+ *
+ *   Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package org.serverless.workflow.validation.testproviders;
+
+import java.util.List;
+
+import org.serverless.workflow.api.WorkflowManager;
+import org.serverless.workflow.api.WorkflowValidator;
+import org.serverless.workflow.api.validation.ValidationError;
+
+public class TestWorkflowValidator implements WorkflowValidator {
+
+    @Override
+    public WorkflowValidator setWorkflowManager(WorkflowManager workflowManager) {
+        return this;
+    }
+
+    @Override
+    public List<ValidationError> validate() {
+        return null;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+
+    }
+
+    @Override
+    public void setSchemaValidationEnabled(boolean schemaValidationEnabled) {
+
+    }
+
+    @Override
+    public void setStrictValidationEnabled(boolean strictValidationEnabled) {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
